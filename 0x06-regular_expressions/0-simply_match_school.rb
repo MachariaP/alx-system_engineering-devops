0a071adf3f5257a5434203796db4977fd56573a1
MachariaP <walburphinehas78@gmail.com>
2024-01-30 12:59:08 +0300

    Add Ruby script for matching 'School' in input

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,13 +1,8 @@
 #!/usr/bin/env ruby
 
 # A regular expression to match the string "School"
-regex = /School/
-
 # Get the input argument from the command line
-input = ARGV[0]
-
 # Use the scan method with the regex to find matches in the input
-matches = input.scan(regex)
-
 # Join the matched results and print the output
-puts matches.join
+
+puts ARGV[0].scan(/School/n).join
