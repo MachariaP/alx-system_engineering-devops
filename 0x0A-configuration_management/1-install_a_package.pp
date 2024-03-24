39a8e0b4a94250d4db752468d5173a183f7daf02
MachariaP <walburphinehas78@gmail.com>
2024-03-24 12:59:49 +0300

    modified a typo in the provider attribute

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,12 @@
-# A puppet manifest installing flask v2.1.0 which is a package from pip3.
-# Am also specifying the version of flask to install using the ensure attribute
+# Using puppet, install flask from pip3.
+# This manifest installs Flask version 2.1.0 using pip3
+
 package { 'flask':
   ensure   => '2.1.0',
-  provider => pip3,
+  provider => 'pip3',
+}
+
+package { 'werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
 }
