67ecdb896af903fb5b8fdd6521fcdb15aa4af1e3
MachariaP <walburphinehas78@gmail.com>
2024-05-07 12:16:04 +0300

    a function that queries the Reddit API and prints the titles of the first 10 hot posts listed for a given subreddit

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,18 +1,17 @@
 #!/usr/bin/python3
-"""
-Function that queries the Reddit API and prints the titles of the first 10 hot posts listed for a given subreddit.
-"""
+'''
+    this module contains the function top_ten
+'''
 import requests
 from sys import argv
 
 
 def top_ten(subreddit):
-    """
-    returns the top 10 hot posts for a given subreddit
-    """
-
-    user = {'User-agent': 'Lizzie'}
-    url = requests.get('https://www.reddit.com/r/{}/hot.json?limit=10'
+    '''
+        returns the top ten posts for a given subreddit
+    '''
+    user = {'User-Agent': 'Lizzie'}
+    url = requests.get('https://www.reddit.com/r/{}/hot/.json?limit=10'
                        .format(subreddit), headers=user).json()
     try:
         for post in url.get('data').get('children'):
@@ -21,5 +20,5 @@ def top_ten(subreddit):
         print(None)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     top_ten(argv[1])
